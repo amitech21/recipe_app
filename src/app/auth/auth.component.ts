e3a4086b044b0df532e238b6f84fc1f06c850a21
abap788 <a.panchal21@gmail.com>
2020-10-24 14:46:26 +0200

    step 288 24 Oct 2020

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent {
+
+  isLoginMode = true;
+
+  constructor() { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+}
