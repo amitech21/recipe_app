688543586bd2dd900949b7c1c1577ea31b2ab20a
abap788 <a.panchal21@gmail.com>
2020-10-26 22:40:59 +0200

    step 303 26 Oct 2020

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
     idToken: string;
@@ -15,9 +16,10 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-    user = new Subject<User>();
-    
-    constructor(private http: HttpClient) {}
+    user = new BehaviorSubject<User>(null);
+    toke: string = null;
+
+    constructor(private http: HttpClient, private router: Router) {}
     
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -47,10 +49,38 @@ export class AuthService {
         }));
     }
 
+    autoLogin() {
+        const userData: {
+        email:string,
+        id: string,
+        _token: string,
+        _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData)
+            return;
+        
+         const loadedUser = new User(
+             userData.email,
+             userData.id,
+             userData._token,
+             new Date(userData._tokenExpirationDate)
+         ); 
+         
+         if(loadedUser.token) {
+             this.user.next(loadedUser);
+         }
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
+    }
+
     private handleAuthentication(email:string,userId:string, token:string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000 ));
         const user = new User(email,userId,token,expirationDate);
-            this.user.next(user);
+        this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component,EventEmitter, Output, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component,EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
-    constructor(private router:Router, private dataStorageService: DataStorageService){}
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    private userSub: Subscription;
+    
+    constructor(private dataStorageService: DataStorageService,
+                private authService: AuthService){}
+    
 
     ngOnInit() {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !user ? false : true;
+            console.log(this.isAuthenticated);
+        });
     }
 
     collapsed = true;
@@ -26,6 +36,14 @@ export class HeaderComponent implements OnInit {
 
     onFetchData() {
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    onLogout() {
+        this.authService.logout();
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
     }
 
 }
