43fa86e9a2f9abf3c1f2ce9d5d883ae45c81ee6e
abap788 <a.panchal21@gmail.com>
2020-10-26 16:26:35 +0200

    step 297 24 Oct 2020

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -25,8 +25,23 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = "Unknown error occured !!!!";
+        ).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAHzxxfhxqs1pBA5Kl5de4pmCrf4Ph8Bqc',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+
+        let errorMessage = "Unknown error occured !!!!";
             
             if(!errorRes.error || !errorRes.error.error)
                 return throwError(errorMessage);
@@ -41,21 +56,25 @@ export class AuthService {
                     errorMessage = "Email already exist.";
                     break;
                 }
+
+                case 'EMAIL_NOT_FOUND': {
+                    errorMessage = "There is no user record corresponding to this identifier. The user may have been deleted.";
+                    break;
+                }
+
+                case 'INVALID_PASSWORD': {
+                    errorMessage = "The password is invalid or the user does not have a password.";
+                    break;
+                }
+
+                case 'USER_DISABLED': {
+                    errorMessage = "The user account has been disabled by an administrator.";
+                    break;
+                }
                     
             }
 
             return throwError(errorMessage);
-        }));
-    }
 
-    login(email: string, password: string) {
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAHzxxfhxqs1pBA5Kl5de4pmCrf4Ph8Bqc',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        );
     }
 }
