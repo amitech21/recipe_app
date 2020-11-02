609415c8b90ee12707fc1dbe94a75810dba0a4af
abap788 <a.panchal21@gmail.com>
2020-11-02 23:18:36 +0200

    step 331 2 Nov 2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,15 @@ import { RecipesComponent } from './recipes/recipes.component';
 
 
 const appRoutes: Routes = [
-  {path: '', component: RecipesComponent}
-  
+  //{path: '', component: RecipesComponent},
+  {path: '', redirectTo: '/auth', pathMatch: 'full'},
+  //{ path: 'recipes', loadChildren: './recipes/recipes.module.ts#RecipesModule' }
+  // If above string giving error in new version, try below code
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+  // shopping-list
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -22,11 +19,8 @@ import { AuthModule } from './auth/auth.module';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
-    CoreModule,
-    AuthModule
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
