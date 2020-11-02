6fc5e10a9323937fb2f2fccf4baeddd0f65073cc
abap788 <a.panchal21@gmail.com>
2020-11-02 22:14:15 +0200

    step 328 2 Nov 2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
-import { AuthComponent } from './auth/auth.component';
 
 
 const appRoutes: Routes = [
-  {path: '', component: RecipesComponent},
-  {path: 'auth', component: AuthComponent},
+  {path: '', component: RecipesComponent}
+  
 ];
 
 @NgModule({
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+    declarations: [
+        AuthComponent
+    ],
+    imports: [RouterModule, CommonModule, SharedModule, FormsModule, AuthRoutingModule]
+})
+export class AuthModule {}
