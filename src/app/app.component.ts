51870f804b6d77ee8964ca784106348253bfd273
abap788 <a.panchal21@gmail.com>
2020-12-12 13:20:22 +0200

    step 382 12 Dec 2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 // import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
 import { Store } from '@ngrx/store';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
+export interface HelloResponseData {
+  msg: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent implements OnInit {
   
   constructor(
-    private store: Store<fromApp.AppState>, 
-    private loggingService: LoggingService) {}
+    private store: Store<fromApp.AppState>
+    ) {}
+  
 
   ngOnInit(): void {
     //this.authService.autoLogin();
